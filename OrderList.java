6b226502bac9b9479d38c1b210407ef9fbcb888a
BjerregaardGG <oliverbjerregaard1@gmail.com>
2024-04-11 10:05:50 +0200

    Added removeOrder()

--- OrderList.java
+++ OrderList.java
@@ -1,5 +1,30 @@
 public class OrderList{ 
 
+Scanner scan = new Scanner(System.in); 
+
+public void removeOrder(){
+
+boolean pizzaFundet = false; 
+
+System.out.println("Hvad er nummeret på pizzaen du gerne vil fjerne?"); 
+   int nummer = scan.nextInt(); 
+
+for(int i=0; i<orderList.size(); i++){
+   Pizza pizza = orderList.get(i); 
+   if(pizza.getID() == nummer){ // i tvivl om denne skal være getID eller getNumber... eller? 
+      orderList.remove(i); 
+      pizzaFundet = true; 
+   System.out.println("Pizza nummer : " + nummer + " er fjernet"); 
+break; 
+   }
+}
+if(!pizzaFundet){
+   System.out.println("Kunne ikke finde pizza : " + nummer); 
+   
+   } 
+  }
+
+
 
 
 }
