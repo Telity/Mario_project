e257e0d605c956ccaed13418c87aa281236088b1
SaltyShaman <103444798+SaltyShaman@users.noreply.github.com>
2024-04-11 10:58:38 +0200

    Update Ui.java

--- Ui.java
+++ Ui.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ui{
-public static void main (String[] args) {
+ //public static void main (String[] args) {
 Scanner scanner = new Scanner(System.in);
 
    while (true) {
