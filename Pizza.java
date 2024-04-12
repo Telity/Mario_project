aacc26a7043fcc89fee1717068297ba508f008a7
BjerregaardGG <oliverbjerregaard1@gmail.com>
2024-04-12 12:32:23 +0200

    ToString update

--- Pizza.java
+++ Pizza.java
@@ -64,9 +64,19 @@ public class Pizza{
       public LocalTime getTime(){
          return time; 
    }
-      public String toString(){ // id name size toppins price time
-         return "Order : Id: ["+ID+"] Pizza: ["+name+"] Size: ["+size+"] Toppings: ["+toppings+"] Price: ["+price+"] Time: ["+time+"]";
-   }//tostring
+     /* public String toString(){ // id name size toppins price time
+         return "Order : Id: ["+ID+"] Pizza: ["+name+"] Size: ["+size+"] Toppings: ["+toppings+"] Price: ["+price+"] Time: ["+time+"]"; */
+         
+        public String toString(){ // id name size toppins price time
+        String menuString = "Menu:\n";
+            for (Pizza pizza : menuList) {
+            menuString += pizza.toString() + "\n";
+    }
+        return menuString;
+    }
+         
+         
+   //tostring
    //an array for the menu
    static ArrayList<Pizza> menuList = new ArrayList<Pizza>();
    // ID Name Size Toppings Price 
