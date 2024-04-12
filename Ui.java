49c2e2a515a820f050e92283974dd65a3c2c048f
SaltyShaman <103444798+SaltyShaman@users.noreply.github.com>
2024-04-12 11:57:57 +0200

    exit gemmer nu en text fil med statistik

--- Ui.java
+++ Ui.java
@@ -13,8 +13,8 @@ public class Ui {
             System.out.println("1: Look at the current orders");
             System.out.println("2: Add order");
             System.out.println("3: Remove order");
-            System.out.println("4: Save sale statistics of the day");
-            System.out.println("5: View menu card");
+            System.out.println("4: View menu card");
+            System.out.println("5: Add pizza to the menu card");
             System.out.println("6: Exit");
 
             int choice = scanner.nextInt();
@@ -26,18 +26,21 @@ public class Ui {
                     break;
                 case 2:
                     orderList.makeOrder();
+                    orderList.showOrder();
                     break;
                 case 3:
                     orderList.removeOrder();
                     break;
                 case 4:
-                    // Call method to save sale statistics of the day
+                    pizza.viewMenu();
                     break;
                 case 5:
-                    pizza.viewMenu();
+                    // Method for adding pizza to menu card
                     break;
                 case 6:
                     System.out.println("Exiting UI. Have a great day");
+                    orderList.statistic();
+                    orderList.fileOrder();
                     System.exit(0);
                 default:
                     System.out.println("Invalid input");
