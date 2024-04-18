ce61fc1266853f37992339aeaec5346a87769dd3
Jhockinn <Tobiln@hotmail.dk>
2024-04-18 10:10:48 +0200

    nice i guess

--- OrderList.java
+++ OrderList.java
@@ -1,79 +1,78 @@
 import java.util.*;
 import java.io.*;
 import java.time.format.*;
-import java.text.*;
 import java.time.*;
 
-public class OrderList{ 
-Pizza menu = new Pizza(); 
-Scanner scan = new Scanner(System.in); 
-static LocalTime time = LocalTime.of(14, 0); // Initialize time to 14:00 / pizzeria opens at 14. 
-static Random random = new Random();
-static int count;
-  
+public class OrderList {
+    Pizza menu = new Pizza();
+    Scanner scan = new Scanner(System.in);
+    static LocalTime time = LocalTime.of(14, 0); // Initialize time to 14:00 (pizzeria opens at 14:00)
+    static Random random = new Random();
+    static int count;
 
-ArrayList<Pizza> orderList = new ArrayList<Pizza>();
-ArrayList<Pizza> savedOrders = new ArrayList<Pizza>();
+    ArrayList<Pizza> orderList = new ArrayList<Pizza>();
+    ArrayList<Pizza> savedOrders = new ArrayList<Pizza>();
 
-// creates a new order (time & pizza ID)
-public void makeOrder(){
-   System.out.println("Is customer calling or ordering in shop (1 for shop, 2 for call)");
-            int type = scan.nextInt();
-            if(type>2 || type<0){
+    // Creates a new order (time & pizza ID)
+    public void makeOrder() {
+        System.out.println("Is the customer ordering in the shop or calling? (1 for shop, 2 for call)");
+        int type = scan.nextInt();
+        if (type > 2 || type < 0) {
             System.out.println("Invalid option!");
-            }
-            while(type == 1 || type == 2){
+        }
+        while (type == 1 || type == 2) {
             if (type == 1) {
                 time = time.plusMinutes(15); // Add 15 minutes
-                } else if(type == 2) {
+            } else if (type == 2) {
                 System.out.println("Hello and thank you for calling");
                 time = time.plusHours(1); // Add 1 hour
-                }
-             
-   System.out.println("How many pizzas is the customer ordering?");
-      int choice = scan.nextInt();
-   
-   // shows the menu 
-   System.out.println("Here is the current menu: "); 
-   Pizza.viewMenu();   
-   System.out.println(" ");    
-      boolean invalid = true;
-      double price = 0;
-         for(int x = 0;x < choice; x++){
-            System.out.println("What number pizza would you like to add to orderlist?");
-            int pizzaID = scan.nextInt()-1;
-            if(pizzaID<0 || pizzaID>menu.menuList.size()){
-            System.out.println("Sorry that pizza is not on the menu, try again");
-            invalid = false;
-            break;
             }
-            
-            
-            Pizza orgPizza = menu.menuList.get(pizzaID);
-               if (orgPizza != null) {
-            // Ensure that the Pizza object is properly initialized
-                  Pizza pizza = new Pizza(orgPizza.getID(),orgPizza.getName(), orgPizza.getSize(), orgPizza.getToppings(), orgPizza.getPrice());
-                  pizza.setTime(time); // Set the time for the pizza order
-            
-            // Add the pizza to both order lists
-                  orderList.add(pizza);
-                  savedOrders.add(pizza);
-            
-            // Calculate the total price
-                  price += pizza.getPrice();
-            }
+            System.out.println("How many pizzas is the customer ordering?");
+            int choice = scan.nextInt();
 
-      }
-      if(invalid == false){
-         break;
-      }
-      System.out.println("Pizzas will be ready at " + time); 
-      System.out.println("The total price is: " + price);
-break;
-   }
-}
-// sorts the pizzas by time 
-public void sortOrder() {
+            // Display the menu
+            System.out.println("Here is the current menu: ");
+            Pizza.viewMenu();
+            System.out.println();
+
+            boolean invalid = true;
+            double price = 0;
+            for (int x = 0; x < choice; x++) {
+                System.out.println("Which pizza number would you like to add to the order list?");
+                int pizzaID = scan.nextInt() - 1;
+                if (pizzaID < 0 || pizzaID >= menu.menuList.size()) {
+                    System.out.println("Sorry, that pizza is not on the menu. Please try again.");
+                    invalid = false;
+                    break;
+                }
+
+                Pizza orgPizza = menu.menuList.get(pizzaID);
+                if (orgPizza != null) {
+                    // Ensure that the Pizza object is properly initialized
+                    Pizza pizza = new Pizza(orgPizza.getID(), orgPizza.getName(), orgPizza.getSize(),
+                            orgPizza.getToppings(), orgPizza.getPrice());
+                    pizza.setTime(time); // Set the time for the pizza order
+
+                    // Add the pizza to both order lists
+                    orderList.add(pizza);
+                    savedOrders.add(pizza);
+
+                    // Calculate the total price
+                    price += pizza.getPrice();
+                }
+
+            }
+            if (!invalid) {
+                break;
+            }
+            System.out.println("Pizzas will be ready at " + time);
+            System.out.println("The total price is: " + price);
+            break;
+        }
+    }
+
+    // Sorts the pizzas by time
+    public void sortOrder() {
         Collections.sort(orderList, new Comparator<Pizza>() {
             @Override
             public int compare(Pizza pizza1, Pizza pizza2) {
@@ -82,64 +81,60 @@ public void sortOrder() {
         });
     }
 
-// prints out the orderlist 
-public void showOrder(){
+    // Prints out the order list
+    public void showOrder() {
+        int actualNumber = 1;
 
-   int actualNumber = 1; 
+        for (Pizza pizza : orderList) {
+            pizza.setActualNumber(actualNumber);
+            System.out.print("Number: " + pizza.getActualNumber());
+            System.out.print(" Name: " + pizza.getName());
+            System.out.print(" Size: " + pizza.getSize());
+            System.out.print(" Toppings: " + pizza.getToppings());
+            System.out.print(" Time: " + pizza.getTime());
+            System.out.println();
+            actualNumber++;
+        }
 
-   for(Pizza pizza : orderList){
-      pizza.setActualNumber(actualNumber); 
-      System.out.print("Number: " + pizza.getActualNumber()); 
-      System.out.print(" Name: " + pizza.getName()); 
-      System.out.print(" Size: " + pizza.getSize()); 
-      System.out.print(" Toppings: " + pizza.getToppings());
-      System.out.print(" Time: " + pizza.getTime()); 
-      System.out.println(); 
-      actualNumber++; 
-   }
-   
-   if(orderList.size() == 0){
-   System.out.println("You're fast! There are no current orders");
-  }
-}
+        if (orderList.size() == 0) {
+            System.out.println("You're fast! There are no current orders");
+        }
+    }
 
-// removes a order from the orderlist 
-public void removeOrder(){
+    // Removes an order from the order list
+    public void removeOrder() {
+        System.out.println("Here is the current order list:");
+        showOrder();
 
-System.out.println("Here is the current orderlist:"); 
-showOrder(); 
+        boolean pizzaFound = false;
 
-boolean pizzaFundet = false;
+        System.out.println("How many pizzas would you like to remove from the order list?");
+        int amount = scan.nextInt();
 
-System.out.println("How many pizzas would you like to remove from the orderlist?"); 
-int antal = scan.nextInt(); 
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Here is the current order list:");
+            showOrder();
+            System.out.println("What is the number of the pizza you want to remove?");
+            int number = scan.nextInt();
 
-int i; 
+            for (int j = 0; j < orderList.size(); j++) {
+                Pizza pizza = orderList.get(j);
+                if (pizza.getActualNumber() == number) {
+                    System.out.println("Pizza number: " + number + " " + orderList.get(number - 1).getName()
+                            + " has been removed");
+                    orderList.remove(number - 1);
+                    pizzaFound = true;
+                    break;
+                }
+            }
 
-for(i=0; i<antal; i++){
-System.out.println("Here is the current orderlist:"); 
-   showOrder(); 
-System.out.println("What is the number of the pizza you want to remove?"); 
-   int nummer = scan.nextInt(); 
+            if (!pizzaFound) {
+                System.out.println("Cannot find pizza: " + number);
+            }
+        }
+    }
 
-for(int j=0; j<orderList.size(); j++){
-   Pizza pizza = orderList.get(j); 
-   if(pizza.getActualNumber() == nummer){ 
-      System.out.println("Pizza number : " + nummer + " " + orderList.get(nummer-1).getName()+" is removed"); 
-      orderList.remove(nummer-1); 
-      pizzaFundet = true; 
-   break; 
-   }
-}
-
-if(!pizzaFundet){
-   System.out.println("Can't find the pizza: " + nummer); 
-   
-    } 
-  }
-}
-
-    // count method which is called in fileOrder, tell how many times a pizza name is on list. 
+    // Count method which is called in fileOrder, tells how many times a pizza name is on the list
     public static int count(ArrayList<Pizza> list, String name) {
         count = 0;
         for (Pizza pizza : list) {
@@ -150,67 +145,67 @@ if(!pizzaFundet){
         return count;
     }
 
-// creates a order - writes todays date - writes individual pizza details - sorts pizza by name and occurrences - statistic
-public void fileOrder(){
-   try{
-      File file = new File("savedOrders.txt");
-         if(!file.exists()){
-            file.createNewFile();
-         }
-      FileWriter writer = new FileWriter(file,true);
-         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy"); // Change date from american system to danish
-         String dkDate = LocalDate.now().format(formatter); //Changes date to a string  
-            writer.write("Todays date: ["+dkDate+"]\n");
-            //sort the pizzas after names
+    // Creates an order - writes today's date - writes individual pizza details - sorts pizza by name and occurrences -
+    // statistic
+    public void fileOrder() {
+        try {
+            File file = new File("savedOrders.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter writer = new FileWriter(file, true);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String dkDate = LocalDate.now().format(formatter);
+            writer.write("Today's date: [" + dkDate + "]\n");
+
+            // Sort the pizzas by names
             Collections.sort(savedOrders, new Comparator<Pizza>() {
-                  @Override
-                     public int compare(Pizza p1, Pizza p2) {
-                        return p1.getName().compareTo(p2.getName());
-                     }
+                @Override
+                public int compare(Pizza p1, Pizza p2) {
+                    return p1.getName().compareTo(p2.getName());
+                }
             });
-            
-            for(Pizza pizza : savedOrders){
-                 writer.write("ID: ["+pizza.getID()+"]  Name: [" + pizza.getName()+ "] Size: ["
-                  + pizza.getSize()+ "] Toppings: [" + pizza.getToppings()+"] Price: [" + pizza.getPrice() + "] Time: [" 
-                  + pizza.getTime()+"]\n");
-                 }
-                 writer.write("\n");
-            //Statistic for pizza, tells pizza name and how many of that type there is.       
+
+            for (Pizza pizza : savedOrders) {
+                writer.write("ID: [" + pizza.getID() + "]  Name: [" + pizza.getName() + "] Size: [" + pizza.getSize()
+                        + "] Toppings: [" + pizza.getToppings() + "] Price: [" + pizza.getPrice() + "] Time: ["
+                        + pizza.getTime() + "]\n");
+            }
+            writer.write("\n");
+
+            // Statistics for pizza, tells pizza name and how many of that type there is
             String[] pizzaNames = new String[savedOrders.size()];
             int[] pizzaCounts = new int[savedOrders.size()];
 
-               int numPizza = 0;
+            int numPizza = 0;
 
-               for (Pizza pizza : savedOrders) {
-               String pizzaName = pizza.getName();
-               boolean isProcessed = false;
-                 // checks if the pizza name is already there
-                 for (int i = 0; i < numPizza; i++) {
-                     if (pizzaNames[i].equals(pizzaName)) {
-                     isProcessed = true;
-                     break;
-                     }
-                     }
-               // if a new pizza name comes, this will add it to the arrays, with both name and a number.
-               // count method, will add how many times that pizza name have occurred, in the savedOrders arraylist. 
-                 if (!isProcessed) {
-                     int count = count(savedOrders, pizzaName);
-                     pizzaNames[numPizza] = pizzaName;
-                     pizzaCounts[numPizza] = count;
-                     numPizza++;
-                     }
-                }  
-      writer.write("Data for Statistics \n");
-      // Writes the name of the pizza and how many times it have been ordered. 
-      for (int i = 0; i < numPizza; i++) {
-          writer.write("Number of Pizza " + pizzaNames[i] + " is : " + pizzaCounts[i] + "\n");
-      }
-      writer.write("\n");
-      writer.close();
-      }
-   catch(Exception e){
-      e.printStackTrace();
-   }
+            for (Pizza pizza : savedOrders) {
+                String pizzaName = pizza.getName();
+                boolean isProcessed = false;
 
-}
+                for (int i = 0; i < numPizza; i++) {
+                    if (pizzaNames[i].equals(pizzaName)) {
+                        isProcessed = true;
+                        break;
+                    }
+                }
+
+                if (!isProcessed) {
+                    int count = count(savedOrders, pizzaName);
+                    pizzaNames[numPizza] = pizzaName;
+                    pizzaCounts[numPizza] = count;
+                    numPizza++;
+                }
+            }
+
+            writer.write("Data for Statistics\n");
+            for (int i = 0; i < numPizza; i++) {
+                writer.write("Number of " + pizzaNames[i] + " ordered: " + pizzaCounts[i] + "\n");
+            }
+            writer.write("\n");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
